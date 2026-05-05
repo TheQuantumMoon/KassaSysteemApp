9b14f706f8f4ffd2b2a05992b6f403bc55ed1a04
TheQuantumMoon <114161742+TheQuantumMoon@users.noreply.github.com>
2026-05-05 18:57:31 +0200

    Added products to Kassa

--- WinkelDomein/Model/Product.cs
+++ WinkelDomein/Model/Product.cs
@@ -1,8 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace WinkelDomein.Model {
+    public class Product {
+        private readonly string _code;
+        private readonly string _name;
+        private readonly decimal _price;
+        private readonly int _btw;
 
-namespace WinkelDomein.Model {
-    internal class Product {
+        public Product(string code, string name, decimal price, int btw = 21) {
+            Code = code;
+            Name = name;
+            Price = price;
+            Btw = btw;
+        }
+
+        public string Code {
+            get => _code;
+            init {
+                ArgumentException.ThrowIfNullOrWhiteSpace(value);
+                _code = value;
+            }
+        }
+        public string Name {
+            get => _name;
+            init {
+                ArgumentException.ThrowIfNullOrWhiteSpace(value);
+                _name = value;
+            }
+        }
+        public decimal Price {
+            get => _price;
+            init {
+                ArgumentOutOfRangeException.ThrowIfLessThan(value, 0m);
+                _price = value;
+            }
+        }
+
+        public int Btw {
+            get => _btw;
+            init {
+                ArgumentOutOfRangeException.ThrowIfLessThan(value, 0);
+                _btw = value;
+            }
+        }
     }
 }
+
